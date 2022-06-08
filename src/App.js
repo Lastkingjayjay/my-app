728dddf52a6a6f292f33b54f164504ccda303909
Lastkingjayjay <juniorjosephr@gmail.com>
2022-06-07 21:52:00 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './Components/home/header/Header'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import HomePages from "./Components/home/home/Homes";
+
+function App() {
+  return (<>
+ 
+  <Router>
+  <Header/>
+  <Switch>
+  <Route path='/' exact component={HomePages}/>
+  </Switch>
+  </Router>
+  </>
+
+  );
+}
+
+export default App;
---
+++ src/Components/pageContent/HomePages.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from '../home/home/Homes'
+const HomePages = () => {
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+
+export default HomePages
